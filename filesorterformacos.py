a43bde4b1a14631112246d9566739780df07ac4c
“bpa812” <braxton.arentz@outlook.com>
2025-04-26 20:15:46 +1000

    Efficiency

--- filesorterformacos.py
+++ filesorterformacos.py
@@ -1,48 +1,46 @@
-import shutil
-import os
+import shutil, os
 checks = [0,0,0,0]
 # folder path
 dir_path = input('Folder Path:')
-# list to store files
-res = []
 # Iterate directory
 for path in os.listdir(dir_path):
     # check if current path is a file
-    foldervar = os.path.join(dir_path, path)
-    if os.path.isfile(foldervar):
-        if foldervar[-4] + foldervar[-3] + foldervar[-2] + foldervar[-1] == '.png':
+    f = os.path.join(dir_path, path)
+    if os.path.isfile(f):
+        l = len(str(f))
+        if f[(-4):(l)] == '.png':
             if checks[0] == 0:
                 path = os.path.join(dir_path, 'Images')
                 os.mkdir(path)
                 checks[0] = 1
-            shutil.move(foldervar, os.path.join(dir_path, 'Images'))
-        elif foldervar[-4] + foldervar[-3] + foldervar[-2] + foldervar[-1] == '.jpg':
+            shutil.move(f, os.path.join(dir_path, 'Images'))
+        elif f[(-4):(l)] == '.jpg':
             if checks[0] == 0:
                 path = os.path.join(dir_path, 'Images')
                 os.mkdir(path)
                 checks[0] = 1
-            shutil.move(foldervar, os.path.join(dir_path, 'Images'))
-        elif foldervar[-5] + foldervar[-4] + foldervar[-3] + foldervar[-2] + foldervar[-1] == '.jpeg':
+            shutil.move(f, os.path.join(dir_path, 'Images'))
+        elif f[(-5):(l)] == '.jpeg':
             if checks[0] == 0:
                 path = os.path.join(dir_path, 'Images')
                 os.mkdir(path)
                 checks[0] = 1
-            shutil.move(foldervar, os.path.join(dir_path, 'Images'))
-        elif foldervar[-4] + foldervar[-3] + foldervar[-2] + foldervar[-1] == '.pdf':
+            shutil.move(f, os.path.join(dir_path, 'Images'))
+        elif f[(-4):(l)] == '.pdf':
             if checks[1] == 0:
                 path = os.path.join(dir_path, 'PDFs')
                 os.mkdir(path)
                 checks[1] = 1
-            shutil.move(foldervar, os.path.join(dir_path, 'PDFs'))
-        elif foldervar[-5] + foldervar[-4] + foldervar[-3] + foldervar[-2] + foldervar[-1] == '.xlsx':
+            shutil.move(f, os.path.join(dir_path, 'PDFs'))
+        elif f[(-5):(l)] == '.xlsx':
             if checks[2] == 0:
                 path = os.path.join(dir_path, 'Excel Spreadsheets')
                 os.mkdir(path)
                 checks[2] = 1
-            shutil.move(foldervar, os.path.join(dir_path, 'Excel Spreadsheets'))
-        elif foldervar[-5] + foldervar[-4] + foldervar[-3] + foldervar[-2] + foldervar[-1] == '.pptx':
+            shutil.move(f, os.path.join(dir_path, 'Excel Spreadsheets'))
+        elif f[(-5):(l)] == '.pptx':
             if checks[3] == 0:
                 path = os.path.join(dir_path, 'PowerPoint Presentations')
                 os.mkdir(path)
                 checks[3] = 1
-            shutil.move(foldervar, os.path.join(dir_path, 'PowerPoint Presentations'))
+            shutil.move(f, os.path.join(dir_path, 'PowerPoint Presentations'))
